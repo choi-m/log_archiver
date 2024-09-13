979b065bce575caaeb29cf9afda3b8bba1a9e54a
choi-m <0101m744@gmail.com>
2024-09-13 13:46:29 -0400

    removing files

--- demo.sh
+++
@@ -1,9 +0,0 @@
-
-
-echo "enter a valid directory" 
-    read -p "directory: " VALUE
-    if [[ -d $VALUE ]]; then 
-        FILE_NAME = "Compressed_Files" 
-        tar cfvz [$FILE_NAME] $VALUE 
-        FILE_LIST = "$VALUE/FILE_NAME" 
-    
